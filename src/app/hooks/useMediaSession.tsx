4f081131bd1582d4035acd16bdc45856e1977d1e
Nico Martin <mail@nico.dev>
2023-02-28 13:52:47 +0100

    some refactoring for the media session api

--- src/app/hooks/useMediaSession.tsx
+++ src/app/hooks/useMediaSession.tsx
@@ -13,33 +13,58 @@ export default ({
   controls,
 }: Props) => {
   React.useEffect(() => {
-    if (!element || !('mediaSession' in navigator)) {
-      return;
+    if (element && 'mediaSession' in navigator) {
+      console.log('set mediaSession', {
+        title,
+        artist,
+        album,
+        artwork: [...artwork],
+      });
+
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title,
+        artist,
+        album,
+        artwork: [...artwork],
+      });
+
+      //@ts-ignore
+      Object.keys(controls).forEach((e: MediaSessionAction) =>
+        navigator.mediaSession.setActionHandler(e, controls[e])
+      );
     }
 
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title,
-      artist,
-      album,
-      artwork: [...artwork],
-    });
-
-    //@ts-ignore
-    Object.keys(controls).forEach((e: MediaSessionAction) =>
-      navigator.mediaSession.setActionHandler(e, controls[e])
-    );
+    return () => {
+      if ('mediaSession' in navigator) {
+        console.log('clean up mediaSession');
+        navigator.mediaSession.metadata = null;
+        //@ts-ignore
+        Object.keys(controls).forEach((e: MediaSessionAction) =>
+          navigator.mediaSession.setActionHandler(e, null)
+        );
+      }
+    };
   }, [element]);
 
   React.useEffect(() => {
-    if (!element || !('mediaSession' in navigator)) {
-      return;
+    if (element && 'mediaSession' in navigator) {
+      console.log('set state', {
+        duration: element.duration || 0,
+        playbackRate: element.playbackRate,
+        position: element.currentTime,
+      });
+      navigator.mediaSession.setPositionState({
+        duration: element.duration || 0,
+        playbackRate: element.playbackRate,
+        position: Math.floor(element.currentTime),
+      });
     }
 
-    navigator.mediaSession.setPositionState({
-      duration: element.duration || 0,
-      playbackRate: element.playbackRate,
-      position: element.currentTime,
-    });
+    return () => {
+      console.log('clean up state');
+      'mediaSession' in navigator &&
+        navigator.mediaSession.setPositionState(null);
+    };
   }, [element?.duration, element?.playbackRate, element?.currentTime]);
 
   return;
